a8d3b9523621490beb3380a16018a2848c41ee47
William So <polyipseity@gmail.com>
2025-10-24 22:02:40 +0800

    Reformat lab 6

--- src/main/java/comp3111/labs/lab6/main.java
+++ src/main/java/comp3111/labs/lab6/main.java
@@ -1,21 +1,21 @@
 package comp3111.labs.lab6;
 
 /*
- * note the bad coding style (deliberately written for debugging exercise) 
+ * note the bad coding style (deliberately written for debugging exercise)
  */
 public class main {
 	public static void main(String[] args) {
 		Animal animals[] = new Animal[10];
 		for (Animal a : animals)
-			a = new Animal();	
+			a = new Animal();
 		for (int iii = 0; iii < 10; iii++) {
 			int ii = 0;
-			for (; ii < 100 && animals[iii].isAlive() ; ii++) {
+			for (; ii < 100 && animals[iii].isAlive(); ii++) {
 				System.out.print(animals[iii].getWeight() + " ");
 				animals[iii].eat();
 				if (ii % 3 == 0)
-				animals[iii].poo();
+					animals[iii].poo();
 			}
-		}	
+		}
 	}
 }
